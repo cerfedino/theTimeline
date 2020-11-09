b447fdd2de99e6aabdf31b860ddf44382c9c357c
Marco Biasion <biasim@usi.ch>
2020-11-09 12:21:07 +0000

    changed how the timeline load the data

---
+++ web/html/timeline/pageMapping.js
@@ -0,0 +1,235 @@
+let pages = {
+  "-1": {
+    "gen#": {
+      "pages": [
+        {
+          "author": "NOME COGNOME", 
+          "date": "2001-01-12", 
+          "description": "Best template ever, long live the CSS LEADERS!!", 
+          "href": "../../html/gen2_adventure/adventure.html", 
+          "picture": "media/placeholder.gif", 
+          "product": "TEMPLATE"
+        }, 
+        {
+          "author": "NOME COGNOME", 
+          "date": "2001-01-12", 
+          "description": "Best template ever, long live the CSS LEADERS!!", 
+          "href": "../../html/gen2_utopia/utopia.html", 
+          "picture": "media/placeholder.gif", 
+          "product": "TEMPLATE"
+        }, 
+        {
+          "author": "NOME COGNOME", 
+          "date": "2001-01-12", 
+          "description": "Best template ever, long live the CSS LEADERS!!", 
+          "href": "../../html/gen4_super_metroid/TEMPLATE.html", 
+          "picture": "media/placeholder.gif", 
+          "product": "TEMPLATE"
+        }, 
+        {
+          "author": "NOME COGNOME", 
+          "date": "2001-01-12", 
+          "description": "Best template ever, long live the CSS LEADERS!!", 
+          "href": "../../html/gen2_missile_command/missile_command.html", 
+          "picture": "media/placeholder.gif", 
+          "product": "TEMPLATE"
+        }, 
+        {
+          "author": "NOME COGNOME", 
+          "date": "2001-01-12", 
+          "description": "Best template ever, long live the CSS LEADERS!!", 
+          "href": "../../html/gen2_space_invaders/space_invaders.html", 
+          "picture": "media/placeholder.gif", 
+          "product": "TEMPLATE"
+        }, 
+        {
+          "author": "NOME COGNOME", 
+          "date": "2001-01-12", 
+          "description": "Best template ever, long live the CSS LEADERS!!", 
+          "href": "../../html/gen2_pac_man/pac_man.html", 
+          "picture": "media/placeholder.gif", 
+          "product": "TEMPLATE"
+        }, 
+        {
+          "author": "NOME COGNOME", 
+          "date": "2001-01-12", 
+          "description": "Best template ever, long live the CSS LEADERS!!", 
+          "href": "../../html/gen4_sonic_the_hedgehog/TEMPLATE.html", 
+          "picture": "media/placeholder.gif", 
+          "product": "TEMPLATE"
+        }
+      ]
+    }, 
+    "invalid": [
+      "../../html/gen5_nintendo_64/nintendo_64.html"
+    ]
+  }, 
+  "gen1": {
+    "pages": [
+      {
+        "author": "Enrico Benedettini", 
+        "date": "1972-05-24", 
+        "description": "A briefly recap about the various first generation genres.", 
+        "href": "../../html/gen1_genres/genres.html", 
+        "picture": "media/placeholder.gif", 
+        "product": "Genres"
+      }, 
+      {
+        "author": "Enrico Benedettini", 
+        "date": "1972-05-24", 
+        "description": "The first home console ever", 
+        "href": "../../html/gen1_magnavox_odyssey/magnavox_odyssey.html", 
+        "picture": "media/magnavox.gif", 
+        "product": "Magnavox Odyssey"
+      }, 
+      {
+        "author": "Enrico Benedettini", 
+        "date": "1972-05-24", 
+        "description": "The main first generation console series.", 
+        "href": "../../html/gen1_odyssey_series/odyssey_series.html", 
+        "picture": "media/magnavox.gif", 
+        "product": "Odyssey Series"
+      }, 
+      {
+        "author": "Enrico Benedettini", 
+        "date": "1975-12-12", 
+        "description": "The first japanese home console ever.", 
+        "href": "../../html/gen1_tv_tennis_electrotennis/tv_tennis_electrotennis.html", 
+        "picture": "media/placeholder.gif", 
+        "product": "TV Tennis Electrotennis"
+      }, 
+      {
+        "author": "Enrico Benedettini", 
+        "date": "1976-01-01", 
+        "description": "The first handheld consoles ever.", 
+        "href": "../../html/gen1_handheld_systems/handheld_systems.html", 
+        "picture": "media/placeholder.gif", 
+        "product": "Handheld Systems"
+      }
+    ]
+  }, 
+  "gen2": {
+    "pages": [
+      {
+        "author": "ALESSANDRO CRAVIOGLIO", 
+        "date": "1976-08-08", 
+        "description": "the page of the console Fairchild channel F", 
+        "href": "../../html/gen2_fairchild_channel_f/fairchild_channel_f.html", 
+        "picture": "media/fairchild_channel_f_square.jpg", 
+        "product": "FAIRCHILD CHANNEL F"
+      }, 
+      {
+        "author": "ALESSANDRO CRAVIOGLIO", 
+        "date": "1979-05-01", 
+        "description": "The console Mattel Intellivision", 
+        "href": "../../html/gen2_intellivision/intellivision.html", 
+        "picture": "media/Intelli_Logo.jpg", 
+        "product": "Intellivision"
+      }
+    ]
+  }, 
+  "gen3": {
+    "pages": [
+      {
+        "author": "Arnaud Fauconnet", 
+        "date": "1986-02-21", 
+        "description": "The first episode of a huge video game series", 
+        "href": "../../html/gen3_the_legend_of_zelda/the_legend_of_zelda.html", 
+        "picture": "media/placeholder.jpg", 
+        "product": "The Legend of Zelda"
+      }
+    ]
+  }, 
+  "gen4": {
+    "pages": [
+      {
+        "author": "Francesco Costa", 
+        "date": "1987-12-18", 
+        "description": "The first installment of the Final Fantasy series", 
+        "href": "../../html/gen4_final_fantasy/final_fantasy.html", 
+        "picture": "media/ff.gif", 
+        "product": "Final Fantasy"
+      }
+    ]
+  }, 
+  "gen5": {
+    "pages": [
+      {
+        "author": "Francesco Costa", 
+        "date": "1989-04-21", 
+        "description": "First console of the Game Boy series, developed by Nintendo", 
+        "href": "../../html/gen5_game_boy/game_boy.html", 
+        "picture": "media/game_boy.gif", 
+        "product": "Game Boy"
+      }, 
+      {
+        "author": "Francesco Costa", 
+        "date": "1996-02-27", 
+        "description": "The first installments of the world famous series of video games: Pok\u00e9mon", 
+        "href": "../../html/gen5_pokemon_red_blue/pokemon_red_blue.html", 
+        "picture": "media/pokemon_red_blue.gif", 
+        "product": "Pok\u00e9mon Red and Blue"
+      }, 
+      {
+        "author": "MARCO VESCIO", 
+        "date": "2001-01-12", 
+        "description": "A webpage about Majora's Mask", 
+        "href": "../../html/gen5_majora_s_mask/majoras_mask.html", 
+        "picture": "media/placeholder.gif", 
+        "product": "MAjora's Mask"
+      }
+    ]
+  }, 
+  "gen6": {
+    "pages": [
+      {
+        "author": "Ludovica Savoia", 
+        "date": "2001-11-21", 
+        "description": "One of the best Super Mario Games", 
+        "href": "../../html/gen6_super_smash_bros/super_smash_bros_melee.html", 
+        "picture": "media/placeholder.gif", 
+        "product": "Super Smash Bros Melee"
+      }
+    ]
+  }, 
+  "gen7": {
+    "pages": [
+      {
+        "author": "Ludovica Savoia", 
+        "date": "2008-06-23", 
+        "description": "One of the best shooting games", 
+        "href": "../../html/gen7_battlefield_bad_company/battlefield_bad_company.html", 
+        "picture": "media/placeholder.gif", 
+        "product": "Battlefield bad company"
+      }, 
+      {
+        "author": "Ntwali Muhizi", 
+        "date": "2020-11-04", 
+        "description": "Third iteration of the halo franchise.. yes third", 
+        "href": "../../html/gen7_halo_2/halo_2_gen7.html", 
+        "picture": "media/Halo_2.gif", 
+        "product": "Halo 2"
+      }, 
+      {
+        "author": "Ntwali Muhizi", 
+        "date": "2020-11-05", 
+        "description": "The elder scrolls v page: pioneer of RPGS", 
+        "href": "../../html/gen7_the_elder_scrolls_ v:_skyrim/the_elder_scrolls_ v:_skyrim_gen7.html", 
+        "picture": "skyrim.gif", 
+        "product": "the elder scrolls v: skyrim"
+      }
+    ]
+  }, 
+  "gen8": {
+    "pages": [
+      {
+        "author": "Arthur Morgan", 
+        "date": "2020-03-23", 
+        "description": "A web page about the 2020 VR game, Half Life: Alyx", 
+        "href": "../../html/gen8_half_life_alyx/half_life_alyx.html", 
+        "picture": "media/placeholder.png", 
+        "product": "Half Life: Alyx"
+      }
+    ]
+  }
+};
