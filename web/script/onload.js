63dbc7488f9b2c935240bfaac88d428582a37050
Marco Biasion <biasim@usi.ch>
2020-11-12 18:01:47 +0000

    added fade_in fade_out to pages music

--- web/script/onload.js
+++ web/script/onload.js
@@ -104,83 +104,121 @@ function setup_meta(){
 
 // ============================================================================= AUDIO
 let audio = null;
-let audio_on = false;
-let audio_image = null;
-let audio_cross = null;
-let audio_button = null;
+let music_on = false;
+let music_image = null;
+let music_cross = null;
+let music_button = null;
 
-let audio_interval = null;
-let audio_volume = 1; // volume in Real : 0 < volume < 1
+let music_interval = null;
+let music_volume = 1; // volume in Real : 0 < volume < 1
+let max_music_volume = 0.5;
 let first_played = false;
 
 function setup_audio(){
   // get the audio button element
-  audio_button = get_element( 'sound-button' );
+  music_button = get_element( 'sound-button' );
   // get the audio images
-  audio_image = get_css_variable( '--soundbutton-image' );
-  audio_cross = get_css_variable( '--soundbutton_cross' );
+  music_image = get_css_variable( '--soundbutton-image' );
+  music_cross = get_css_variable( '--soundbutton_cross' );
   // we get the raw audio variable from css { url("dir/file") }
-  let raw_audio_file = get_css_variable( '--audio-file' );
+  let raw_music_file = get_css_variable( '--audio-file' );
   // we extract only the path
-  raw_audio_file = raw_audio_file.split('"')[1];
+  raw_music_file = raw_music_file.split('"')[1];
+
   // we create the audio object from that file
-  audio = new Audio( raw_audio_file );
+  audio = new Audio( raw_music_file );
+  // set other stuff
+  audio.setAttribute( 'loop', '');
+  music_volume = 0.03;
+  audio.volume = 0.03;
 
   // set the on error function to audio
   audio.onerror = () => {
     // if an error happened
     // remove the audio button
-    audio_button.style.display = "none";
+    music_button.style.display = "none";
     // print a message to the console
     console.error("There was an error loading the audio file.\nAudio button hidden to prevent further errors.");
   }
 
   // link the start/stop of the music to the sound-button
-  audio_button.onclick = () => {
-    audio_on = !audio_on;
+  music_button.onclick = () => {
+    music_on = !music_on;
 
     // turn the music on
-    if(audio_on) {
-      audio.play();
-      audio_button.removeAttribute("muted");
+    if(music_on) {
 
       // if first time playing, fade in
       if( !first_played ){
-        audio_volume = 0;
-        // use the function defined after this to fade in in 8000 milliseconds
-        fade_in(8000);
+        fade_in(4000);
         first_played = true;
+
+      } else {
+        fade_in(500);
       }
+
+      music_button.removeAttribute("muted");
 
     // turn the music off
     } else {
-      audio.pause();
-      audio_button.setAttribute("muted", true);
+      fade_out(500);
+      music_button.setAttribute("muted", true);
     }
   }
 
   // this function will make the music fade in
   // it takes 'millis' milliseconds to finish
   function fade_in( millis ){
-    let interval = millis * 0.01;
+    let interval = millis * 0.01 / max_music_volume;
+
+    audio.play();
 
     // set the interval to execute the function each n milliseconds
-    audio_interval = setInterval(frame, interval);
+    clearInterval( music_interval );
+    music_interval = setInterval(frame, interval);
+
     function frame() {
+      // console.log( music_volume );
+
       // always increase the volume
-      audio_volume += 0.01;
+      music_volume += 0.01;
       // if the maximum (1) is reached, crop it to 1 and clear the interval
-      if (audio_volume >= 1) {
-        audio_volume = 1; // to remove excess floating
-        clearInterval(audio_interval);
+      if (music_volume >= max_music_volume) {
+        music_volume = max_music_volume; // to remove excess floating
+        clearInterval(music_interval);
       }
       // set the volume of the object to our volume
-      audio.volume = audio_volume;
+      audio.volume = music_volume;
+    }
+  }
+
+  // this function will make the music fade out
+  // it takes 'millis' milliseconds to finish
+  function fade_out( millis ){
+    let interval = millis * 0.01 / max_music_volume;
+
+    // set the interval to execute the function each n milliseconds
+    clearInterval( music_interval );
+    music_interval = setInterval(frame, interval);
+
+    function frame() {
+      // console.log(music_volume);
+
+      // always increase the volume
+      music_volume -= 0.01;
+      // if the maximum (1) is reached, crop it to 1 and clear the interval
+      if (music_volume <= 0) {
+        music_volume = 0; // to remove excess floating
+        clearInterval(music_interval);
+        audio.pause();
+      }
+      // set the volume of the object to our volume
+      audio.volume = music_volume;
     }
   }
 
   // initialize the state of the audio
-  audio_button.setAttribute("muted", true);
+  music_button.setAttribute("muted", true);
 }
 
 // ============================================================================= BACK BUTTON
